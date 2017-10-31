d524d4ced916ade975902119708763fc5c008c1a
Jag Reehal <jag.reehal@gmail.com>
2017-10-31 08:41:03 +0000

    updated wallaby config and deps

--- wallaby.js
+++ wallaby.js
@@ -1,42 +1,25 @@
-const folders = ["src"];
-const files = ({
-    prefix,
-    ext
-}) => folders.map(folder => {
-    return `${prefix || ""}${folder}/**/*.${ext}`;
-});
-
-module.exports = function (wallaby) {
-    return {
-        debug: true,
-        files: 
-            ['package.json', 'tsconfig.json'].concat
-            (files({
-                ext: "ts*"
-            }))
-            .concat(files({
-                ext: "snap"
-            }))
-            .concat(files({
-                ext: "spec.ts*",
-                prefix: "!"
-            })),
-        tests: files({
-            ext: "spec.ts*"
-        }),
-        env: {
-            type: "node",
-            runner: "node"
-        },
-        compilers: {
-            "**/*.ts*": wallaby.compilers.typeScript({
-                typescript: require("typescript"),
-                module: 'commonjs'
-            })
-        },
-        testFramework: "jest",
-        setup: function () {
-            wallaby.testFramework.configure(require('./package.json').jest);
-        }
-    };
+module.exports = function(wallaby) {
+  return {
+    debug: false,
+    files: [
+      'package.json',
+      'tsconfig.json',
+      '!src/**/*.spec.ts',
+      'src/**/*.ts'
+    ],
+    tests: ['src/**/*.spec.ts'],
+    env: {
+      type: 'node',
+      runner: 'node'
+    },
+    compilers: {
+      '**/*.ts*': wallaby.compilers.typeScript({
+        typescript: require('typescript')
+      })
+    },
+    testFramework: 'jest',
+    setup: function() {
+      wallaby.testFramework.configure(require('./package.json').jest);
+    }
+  };
 };
